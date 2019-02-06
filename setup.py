aac81eed8edb14c10290e8b6e057d5fd219bc17c
David Ingraham <davingrahamd@gmail.com>
2019-02-06 15:45:04 -0700

    Mark Version Number for EFI Version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except LookupError:
 from setuptools import setup, Extension
 import glob, os, shutil, fnmatch, platform, sys
 
-version = '2.2.3'
+version = '92.92.93'
 
 
 def generate_content():
